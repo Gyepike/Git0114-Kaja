ebfc5d12181ab0bbed3a94b16ca7ec92fefd3cec
gyepike <antipet@gmail.com>
2023-01-14 15:18:04 +0100

    Keszen van KAJA LISTA

--- app.js
+++ app.js
@@ -9,10 +9,15 @@ window.addEventListener("load", async () => {
 
     let ouput = "";
 
-    data.map(car => {
+    data.map(kaja => {
         ouput += `
                  <div class= "car">
-                     <h2>Pizza : ${car.pizza} </h2>
+                     <h2>Pizza : ${kaja.id} </h2>
+                     <h2>Pizza : ${kaja.pizza} </h2>
+                     <h2>Pizza : ${kaja.Evjarat} </h2>
+                     <h2>Pizza : ${kaja.Extra} </h2>
+                     <h2>Pizza : ${kaja.Ar} </h2>
+
                  </div>
             `
             ;
